48abcf5949dfa11a7770830d10a4ae772a9a8192
= <=>
2020-04-17 17:53:14 +0900

    주말 과제 용

---
+++ a008_practice/src/main/java/com/lec/android/a008_practice/Profile.java
@@ -0,0 +1,42 @@
+package com.lec.android.a008_practice;
+
+import java.io.Serializable;
+
+public class Profile implements Serializable {
+    String name;
+    String addr;
+    int age;
+
+    public Profile() {
+    }
+
+    public Profile(String name, String addr, int age) {
+        this.name = name;
+        this.addr = addr;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ a008_practice/src/main/java/com/lec/android/a008_practice/ProfileAdapter.java
@@ -0,0 +1,82 @@
+package  com.lec.android.a008_practice;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+    static ProfileAdapter adapter;
+    //profile을 담을 리스트
+    List<Profile> items = new ArrayList<Profile>();
+
+    public ProfileAdapter() {this.adapter = this;
+    }//기본 생성자
+
+    @NonNull
+    @Override
+    public ProfileAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inf = LayoutInflater.from(parent.getContext());
+
+        View itemView = inf.inflate(R.layout.item, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileAdapter.ViewHolder holder, int position) {
+        Profile item = items.get(position);     //arraylist의 함수 get
+        holder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName, tvAddr, tvAge;
+        ImageButton btnDel;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvAddr = itemView.findViewById(R.id.tvAddr);
+            tvAge = itemView.findViewById(R.id.tvAge);
+            btnDel = itemView.findViewById(R.id.btnDel);
+            // VIEW 객체 가져오기
+
+            btnDel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapter.removeItem(getAdapterPosition()); //==> 데이터 삭제
+                    adapter.notifyDataSetChanged(); // 이거한번 이면 가능 꼭해줘야함******8
+
+                }
+
+            });
+        }
+            public void setItem(Profile item){
+
+                tvName.setText(item.getName());
+                tvAddr.setText(item.getAddr());
+                tvAge.setText(item.getAge());
+            }
+
+
+    }//end ViewHolder
+    public void addItem(Profile item) {  items.add(item); }
+    public void addItem(int position, Profile item) {   items.add(position, item);}
+    public void setItems(ArrayList<Profile> items) {   this.items = items;}
+    public Profile getItem(int position) {   return items.get(position);}
+    public void setItem(int position, Profile item) {   items.set(position, item); }
+    public void removeItem(int position){ items.remove(position); }
+
+}
