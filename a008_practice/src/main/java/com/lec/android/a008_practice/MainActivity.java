056751c715721c809f850bd403a2d77af3d255b6
dnd194 <dnd194@gmail.com>
2020-04-18 16:19:43 +0900

    android.content.res.Resources$NotFoundException: String resource ID

--- a008_practice/src/main/java/com/lec/android/a008_practice/MainActivity.java
+++ a008_practice/src/main/java/com/lec/android/a008_practice/MainActivity.java
@@ -7,15 +7,23 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     ProfileAdapter adapter;
     RecyclerView rv;
+    static EditText et,et2,et3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et = findViewById(R.id.et);
+        et2 = findViewById(R.id.et2);
+        et3 = findViewById(R.id.et3);
+
 
         rv = findViewById(R.id.rv);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -35,8 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
     protected void appendData(View v){
-
-        adapter.addItem(new Profile(Profile.name, Profile.addr, Profile.age));
+        Profile.name = et.getText().toString().trim();
+        Profile.addr = et3.getText().toString().trim();
+        Profile.age  = Integer.parseInt(et2.getText().toString().trim());
+        adapter.addItem(0,new Profile(Profile.name, Profile.addr, Profile.age));
         adapter.notifyDataSetChanged();  // 추가했으니 리스트에  *** 반영을 해야함 ***
     }
 }
