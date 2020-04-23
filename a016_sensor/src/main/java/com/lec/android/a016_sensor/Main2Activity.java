b7f4b97432464223382a9655576476a8264d3e1a
dnd194 <dnd194@gmail.com>
2020-04-23 17:43:15 +0900

    4월 23일 수업 끝 location

--- a016_sensor/src/main/java/com/lec/android/a016_sensor/Main2Activity.java
+++ a016_sensor/src/main/java/com/lec/android/a016_sensor/Main2Activity.java
@@ -51,9 +51,40 @@ public class Main2Activity extends AppCompatActivity implements SensorEventListe
         sm.unregisterListener(this);  // 자기자신 반납
     }
 
+    float[] mGravity;
+    float[] mGeomagnetic;
+
     //SensorEventListener 객체의 메소드들
     @Override   //센서 값이 변경될 때마다 호출되는 콜백
     public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity =event.values;             // 센서 값들은 float[] 로 넘어옴
+        if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null){
+            float [] R = new float[9];
+            float [] I = new float[9];
+            boolean success =SensorManager.getRotationMatrix(R,I,mGravity,mGeomagnetic);
+            if(success){
+                float[] orientation = new float[3];
+                SensorManager.getOrientation(R, orientation);
+
+                float azimuth = orientation[0];     //z 축 회전방향
+                float pitch = orientation[1];       //x 축 회전방향
+                float roll = orientation[2];        //y 축 회전방향
+
+                String str= String.format("%10s : %10s : %10s \n%10.2f : %10.2f : %10.2f\n",
+                                         "방위각","피치","롤",azimuth, pitch,roll);
+                try {
+                    Thread.sleep(3000);
+                tv.setText(str);
+                Log.d("myapp",str);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         tv.setText("onSensorChanged");
         Log.d("myapp","onSensorChanged");
     }
@@ -63,5 +94,5 @@ public class Main2Activity extends AppCompatActivity implements SensorEventListe
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         tv.setText("onAccuracyChanged");
         Log.d("myapp","onAccuracyChanged");
-    }
+    }//end onAccuracyChanged
 }//end Activity
