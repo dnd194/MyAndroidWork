64b96a4741ab8744ffda5f9c4b7504ab13134bf6
dnd194 <dnd194@gmail.com>
2020-04-28 00:59:18 +0900

    개인과제용

--- amu/src/main/java/com/lec/android/amu/MainActivity.java
+++ amu/src/main/java/com/lec/android/amu/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 // 인증키 6577656a42646e6437366b496a4343
 // http://openapi.seoul.go.kr:8088/6577656a42646e6437366b496a4343/json/MonthlyAverageAirQuality/1/5/200406  json 형식
@@ -14,18 +18,35 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     RecyclerView rv;
     AirAdapter adapter; //
+    Button btnSearch;
+    EditText etDate;
+    EditText etRegion;
+    final String JsonUrl = "http://openapi.seoul.go.kr:8088/6577656a42646e6437366b496a4343/json/MonthlyAverageAirQuality/1/5/";
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSearch = findViewById(R.id.btnSearch);
+        etDate = findViewById(R.id.etDate);
+        etRegion = findViewById(R.id.etRegion);
         rv = findViewById(R.id.rv);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 //        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);  //그리드 레이아웃
         rv.setLayoutManager(layoutManager);
 
         adapter = new AirAdapter();
-        initAdapter(adapter);
+        initAdapter(adapter);       //데이터 확인용
         rv.setAdapter(adapter);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String comUrl = JsonUrl+etDate.getText().toString().trim()+"/"+etRegion.getText().toString().trim();
+                // 날짜와 지역구 모두 포함된 url
+
+            }
+        });
 
     }//end OnCreate
 
